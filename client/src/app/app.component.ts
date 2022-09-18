095c1b2533ff0f1a2d1bbec02d2cb15253b8fc54
Jitu Surve <jitusurve@gmail.com>
2022-09-18 21:59:53 +0530

    upgraded to Angular 13

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './models/user';
 import { AccountService } from './services/account.service';
-import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
